0b4a6a1b959fd075fed1a6e643b9ae5ff5512be0
m98lima <m98lima@gmail.com>
2025-10-20 16:24:04 -0300

    Update [class] Input Handler.gd

--- IM-M scripts/[class] Input Handler.gd
+++ IM-M scripts/[class] Input Handler.gd
@@ -1,19 +1,10 @@
 extends Resource
 class_name Input_Handler;
 
-@export var mesh_name: String;
-var mesh: Node3D;
 var node_refs: Node_Refs;
 
 func _assign_refs(_node_refs: Node_Refs) -> void:
 	node_refs = _node_refs;
-	mesh = node_refs.meshes.find_child(mesh_name);
-	if (mesh == null):
-		push_error("Mesh name wrong on %s, can't find mesh" % self);
-
-	for character_mesh in node_refs.meshes.get_children():
-		character_mesh.visible = false;
-	mesh.visible = true;
 
 func _setup() -> void:
 	pass;
